685119717149e297d9ca81e3ba228ab66c0e0333
Guibzy <guigz94@gmail.com>
2014-12-10 16:15:49 +0100

    correction bug connexion

--- modules/Connexion/Connexion.module.php
+++ modules/Connexion/Connexion.module.php
@@ -3,38 +3,28 @@ class Connexion extends Module{
 			
 
 	public function action_login(){
-
-		// A FAIRE
-		// verifier les donnees de connexion
-		//charger le membre
-		//$user=Membre::chercherParId(/*mettre l'id*/);
-		//$this->session->ouvrir($user);
-		
-		//code de demo
 		
 		if(MembreManager::chercherParLogin($this->req->Login))
 		{
-			$membre_auth= new Membre();
+			$membre_auth=new Membre();
 			$membre_auth=MembreManager::chercherParLogin($this->req->Login);
 			
 			if($membre_auth->pass == $this->req->Pass)
-			{
-				$this->session->user=$membre_auth;
-				$this->tpl->assign('login',$membre_auth->login);
+			{	
+				$this->session->ouvrir($membre_auth); 
+				$this->tpl->assign('login',$this->req->Login);
+				
 				$this->site->ajouter_message("Vous êtes connecté en tant que ".$membre_auth->login);
-				$this->site->redirect("index");
+				
 			}
 			else
 			{
-			
-				$this->site->redirect("index");
-				$this->site->ajouter_message("Mauvais identifiants et/ou mot de passe ");
 				
+				$this->site->ajouter_message("Mauvais identifiants et/ou mot de passe ",ALERTE);
 			}
 		}
-		
-		
-		
+		$this->site->ajouter_message("Mauvais identifiants et/ou mot de passe ",ALERTE);
+		$this->site->redirect("index");
 	}
 
 	public function action_deconnect(){		
